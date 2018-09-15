8ebc4e855783d21b3163677271b486d257d494a0
SenthilGKML <42924814+SenthilGKML@users.noreply.github.com>
2018-09-15 07:04:32 +0530

    Rename adv_dictionary1.py to add_dictionary_in_list.py

---
+++ add_dictionary_in_list.py
@@ -0,0 +1,20 @@
+#Adding Dictionaries directly in list without using append
+
+books_dic_list = [
+			{
+			'book1':'who will cry when you die',
+			'author':'Robin Sharma',
+			},
+			{
+			'book2':'The Monk who sold his Ferrari',
+			'author':'Robin Sharma',
+			},
+		
+    ]
+
+# show all info about each book
+
+for book_dic in books_dic_list :
+	for k, v in book_dic.items():
+		print(k +":" +v)
+	print("\n")
